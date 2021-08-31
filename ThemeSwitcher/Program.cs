9b8598e2aae5f76648780c433efe2b3c8609c6a0
Burak Türker <65267673+lewenna@users.noreply.github.com>
2021-08-31 18:19:10 +0300

    Wallpaper support!

--- ThemeSwitcher/Program.cs
+++ ThemeSwitcher/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace ThemeSwitcher
@@ -27,7 +28,7 @@ namespace ThemeSwitcher
                 {
                     SetCurrentTheme(Theme.DARK);
                 }
-                Thread.Sleep(30000);
+                Thread.Sleep(5000);
             }
         }
 
@@ -36,10 +37,37 @@ namespace ThemeSwitcher
             return (int)Registry.GetValue(keyName, "SystemUsesLightTheme", null) == 0 ? Theme.DARK : Theme.WHITE;
         }
 
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SystemParametersInfo(uint uiAction, uint uiParam, String pvParam, uint fWinIni);
+
+        private const uint SPI_SETDESKWALLPAPER = 0x14;
+        private const uint SPIF_UPDATEINIFILE = 0x1;
+        private const uint SPIF_SENDWININICHANGE = 0x2;
+
+        public static void SetWallpaper(Theme theme)
+        {
+            uint flags = 0;
+            if (theme == Theme.DARK)
+            {
+                string file = System.Reflection.Assembly.GetExecutingAssembly().Location.Replace("ThemeSwitcher.dll", "") + "dark.png";
+                SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, file, flags);
+                Console.WriteLine(file);
+            }
+            else
+            {
+                string file = System.Reflection.Assembly.GetExecutingAssembly().Location.Replace("ThemeSwitcher.dll", "") + "white.png";
+                SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, file, flags);
+                Console.WriteLine(file);
+
+            }
+        }
+
         public static void SetCurrentTheme(Theme theme) //0 dark, 1 white
         {
             Registry.SetValue(keyName, "AppsUseLightTheme", ThemeValue.GetValue(theme));
             Registry.SetValue(keyName, "SystemUsesLightTheme", ThemeValue.GetValue(theme));
+            SetWallpaper(theme);
         }
     }
 }
